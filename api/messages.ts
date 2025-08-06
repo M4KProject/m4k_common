b7c5d9abbbc2a24dd167e8fa016dbcf535c44e90
Benoit ADAM <b.adam.info@gmail.com>
2025-08-06 16:20:46 +0200

    correction apiUrl

--- api/messages.ts
+++ api/messages.ts
@@ -4,14 +4,12 @@ import { isEmpty, isObject, isString } from "../helpers/check";
 import { toDate } from "../helpers/cast";
 import { UserModel } from "./models";
 
-export const PB_URL_DEV = "http://127.0.0.1:8090/api/";
-export const PB_URL_PROD = "https://pb.m4k.fr/api/";
+// export const PB_URL_DEV = "http://127.0.0.1:8090/api/";
 
 export interface Auth extends UserModel { token: string; }
 export const auth$ = new Msg<Auth | null>(null, "auth", true, isObject);
 
 export const apiUrl$ = new Msg<string>("", "apiUrl", true, isString);
-if (!apiUrl$.v) apiUrl$.set(PB_URL_DEV);
 
 export const timeOffset$ = new Msg(0, "timeOffset", true);
 export const getTimeOffset = timeOffset$.getter();
@@ -22,7 +20,9 @@ export const toTime = (date: any) => toDate(date).getTime();
 
 export const groupId$ = new Msg<string>("", "groupId", true, isString);
 
-export const getApiUrl = apiUrl$.getter();
+const defaultUrl = (location.port ? location.origin.replace(location.port, "8090") : location.origin) + "/api/";
+
+export const getApiUrl = () => apiUrl$.v || defaultUrl;
 export const getAuthId = () => auth$.v?.id || "";
 export const getGroupId = () => groupId$.v;
 
