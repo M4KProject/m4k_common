382a71f5e6587ee5bfab6e4db40fece0a12f0496
Benoit ADAM <b.adam.info@gmail.com>
2025-09-09 14:55:52 +0200

    stringify without error

--- helpers/json.ts
+++ helpers/json.ts
@@ -1,7 +1,46 @@
-import { catcher } from './err';
+import { isItem, isList, Item, List } from "./check";
 
-export const stringify = catcher(JSON.stringify, '');
+export const parse = (text: string) => {
+    try {
+        return JSON.parse(text);
+    } catch (e) {
+        return e;
+    }
+};
 
-export const parse = catcher(JSON.parse);
+export const stringify = (value: any, replacer?: (this: any, key: string, value: any) => any, space?: string | number): string => {
+    try {
+        return JSON.stringify(value, replacer, space);
+    } catch (_) {}
 
-export const clone = <T>(value: T) => parse(stringify(value)) as T;
+    try {
+        if (isList(value)) {
+            const list: List = [];
+            for (const child of value) {
+                try {
+                    JSON.stringify(child);
+                    list.push(child);
+                } catch (e) {
+                    list.push(String(child));
+                }
+            }
+            return JSON.stringify(list, null, space);
+        }
+
+        if (isItem(value)) {
+            const item: Item = {...value};
+            for (const key in value) {
+                try {
+                    JSON.stringify(value[key]);
+                } catch (e) {
+                    item[key] = String(value[key]);
+                }
+            }
+            return JSON.stringify(item, replacer, space);
+        }
+    } catch (_) {}
+
+    return String(value);
+};
+
+export const cloneJson = <T>(value: T) => parse(stringify(value)) as T;
