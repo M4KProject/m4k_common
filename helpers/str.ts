6888475bf1c690aaee3cb2c023b4ea62475bef66
Benoit ADAM <b.adam.info@gmail.com>
2025-09-11 10:55:14 +0200

    randStr

--- helpers/str.ts
+++ helpers/str.ts
@@ -76,29 +76,34 @@ export const replace = (val: string, replaceBySearch: Record<string, any>): stri
 export const setTemplate = (template: string, replaceByKey: Record<string, any>): string =>
   template.replace(/\{(\w+)\}/g, (s, k) => replaceByKey[k] || s);
 
-export const uuid = (): string => {
-  if (typeof crypto === 'object') {
-    if (crypto.randomUUID) return crypto.randomUUID();
-    if (crypto.getRandomValues) {
-      const buff = new Uint16Array(8);
-      crypto.getRandomValues(buff);
-      const S = (i: number) => buff[i].toString(16).padStart(4, '0');
-      return S(0) + S(1) + '-' + S(2) + '-' + S(3) + '-' + S(4) + '-' + S(5) + S(6) + S(7);
+export const randStr = (count: number, chars: string = "abcdefghjkmnpqrstuvwxyz23456789") => {
+  const charset = chars.split('');
+  let result = '';
+  
+  if (typeof crypto === 'object' && crypto.getRandomValues) {
+    const buff = new Uint32Array(count);
+    crypto.getRandomValues(buff);
+    for (let i = 0; i < count; i++) {
+      result += charset[buff[i] % charset.length];
+    }
+  } else {
+    for (let i = 0; i < count; i++) {
+      result += charset[Math.floor(Math.random() * charset.length)];
     }
   }
-  const h = '0123456789abcdef';
-  const k = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
-  let u = '',
-    i = 0,
-    rb = (Math.random() * 0xffffffff) | 0;
-  while (i++ < 36) {
-    const c = k[i - 1],
-      r = rb & 0xf,
-      v = c == 'x' ? r : (r & 0x3) | 0x8;
-    u += c == '-' || c == '4' ? c : h[v];
-    rb = i % 8 == 0 ? (Math.random() * 0xffffffff) | 0 : rb >> 4;
+  
+  return result;
+}
+
+export const randPass = (count: number) => randStr(count, "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789_@#()[]:;,?!=+");
+export const randHex = (count: number) => randStr(count, "0123456789abcdef");
+
+export const uuid = (): string => {
+  if (typeof crypto === 'object' && crypto.randomUUID) {
+    return crypto.randomUUID();
   }
-  return u;
+  const h = randHex;
+  return `${h(8)}-${h(4)}-4${h(3)}-8${h(3)}-${h(12)}`;
 };
 
 /**
