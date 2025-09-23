14f0f915fa265852ba2dcd9bf610017a8aafb72b
Benoit ADAM <b.adam.info@gmail.com>
2025-09-23 02:25:11 +0200

    req isXhr

--- utils/req.ts
+++ utils/req.ts
@@ -278,7 +278,9 @@ const _req = async <T>(options?: ReqOptions<T>): Promise<T> => {
 
   await retry(async () => {
     try {
-      const request = o.request || (o.fetch || typeof fetch === 'function' ? reqFetch : reqXHR);
+      const isXhr = o.xhr || (typeof fetch !== 'function' && typeof o.fetch !== 'function');
+      console.info('isXhr', isXhr);
+      const request = o.request || (isXhr ? reqXHR : reqFetch);
       await request(ctx as any);
       if (o.cast) ctx.data = await o.cast(ctx);
       if (o.after) await o.after(ctx);
