f12c73fa2117b99196b7f814f4449dfbd638010c
Benoit ADAM <b.adam.info@gmail.com>
2025-10-03 11:39:19 +0200

    auto

--- utils/list.ts
+++ utils/list.ts
@@ -102,8 +102,9 @@ export const sortItems = <T = any>(
 
 export const filterItems = <T = any>(
   items: T[],
-  predicate: (value: T, index: number, array: T[]) => boolean
+  predicate?: (value: T, index: number, array: T[]) => boolean
 ) => {
+  if (!predicate) return items;
   const next = items.filter(predicate);
   if (next.length === items.length) return items;
   items.length = 0;
