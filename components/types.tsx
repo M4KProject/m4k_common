d24f82582e809927b45bbf6c537089f61edd286f
BenoitAdam <b.adam.info@gmail.com>
2025-11-04 15:45:00 +0100

    CssStyle

--- components/types.tsx
+++ components/types.tsx
@@ -1,7 +1,7 @@
-import { CSSProperties, JSX } from 'preact';
+import { CssStyle } from 'fluxio';
+import { JSX } from 'preact';
 
-export type Style = CSSProperties;
-export type GetProps<T> = Omit<T, 'style'> & { style?: Style };
+export type GetProps<T> = Omit<T, 'style'> & { style?: CssStyle };
 
 export type Props = {
   [K in keyof JSX.IntrinsicElements]: GetProps<JSX.IntrinsicElements[K]>;
